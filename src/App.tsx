75de6f307555046dea919fdd165d71f9c8905386
Flavio Silva <55815265+201flaviosilva@users.noreply.github.com>
2023-04-10 21:37:54 +0100

    feat: setting up redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
+import { Provider } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import { store } from "./app/store";
 import Account from "./Pages/Account";
 import Home from "./Pages/Home";
 import NotFound from "./Pages/NotFound";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index path="/" element={<Home />} />
-        <Route path="account" element={<Account />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route index path="/" element={<Home />} />
+          <Route path="account" element={<Account />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
