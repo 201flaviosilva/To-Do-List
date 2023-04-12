1f30b3f32826c4e316779314843a67cb76c126e0
Flavio Silva <55815265+201flaviosilva@users.noreply.github.com>
2023-04-12 23:03:15 +0100

    organize project

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import Counter from "../../components/Counter";
+
+export default function Home() {
+	return (
+		<div className="App">
+			<h1>Vite + TypeScript + React + Redux + React Router Dom</h1>
+			<Counter />
+		</div>
+	);
+}
